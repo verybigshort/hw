bebf03cbfd4bdc4526ba1113bd8eaa34762a93b5
verybigshort <113053267+verybigshort@users.noreply.github.com>
2022-09-14 20:48:18 +0300

    Update zadanie8.php

--- zadanie8.php
+++ zadanie8.php
@@ -1,6 +1,6 @@
 <?php
-$array1 = array(1, 5, -7, -13, 8, 2, 2, 4, 10);
-$array2 = array(8, 5, 7, -6, -3, 2, -10, -15, 8);
+$array1 = [1, 5, -7, -13, 8, 2, 2, 4, 10];
+$array2 = [8, 5, 7, -6, -3, 2, -10, -15, 8];
 $arraySum = array_merge($array1,$array2);
 $elementsSum = array_sum($arraySum);
 echo 'Сумма элементов массива: ' . $elementsSum;
