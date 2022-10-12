7241c9ae5c1fc937d4732923f732fd38f0b4aa77
verybigshort <113053267+verybigshort@users.noreply.github.com>
2022-10-12 22:16:45 +0300

    Add files via upload
    Game of Life v0.1

---
+++ space.php
@@ -0,0 +1,96 @@
+<?php
+$space = [
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,1,0,0,0,0,0,0],
+		[0,1,0,1,0,0,0,0,0,0],
+		[0,0,1,1,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0],
+	];
+	
+const ALIVE = 1;
+const DEAD = 0;
+
+const GAME_RUNNING = 1;
+const GAME_STOPPED = 0;
+
+$gameStatus = GAME_RUNNING;
+
+function printSpace($space){
+	foreach($space as $y){
+		foreach($y as $x){
+			if ($x == ALIVE){
+				echo "#";
+			}
+			if ($x == DEAD){
+				echo ".";
+			}
+		}
+		echo PHP_EOL;
+	}
+	echo PHP_EOL;
+}
+
+function countAliveNear($y,$x,$space){
+		$nearSum = [
+			$space[$y-1][$x-1],
+			$space[$y-1][$x],
+			$space[$y-1][$x+1],
+			$space[$y][$x-1],
+			$space[$y][$x+1],
+			$space[$y+1][$x-1],
+			$space[$y+1][$x],
+			$space[$y+1][$x+1]			
+			];
+	return array_sum($nearSum);
+}
+
+
+function evaluate($space){
+	$spaceEvaluated = $space;
+	foreach($space as $y => $yCells){
+		foreach($yCells as $x => $cellStatus){
+			if(0<$y and $y<9 and 0<$x and $x<9){
+				$nearCells = countAliveNear($y,$x,$space);
+				$spaceEvaluated[$y][$x] = cellEvaluate($y,$x,$cellStatus,$nearCells);
+			}
+		}
+	}
+	return $spaceEvaluated;
+}
+	
+function cellEvaluate($y,$x,$cellStatus,$nearCells){
+	if($cellStatus == 1){
+		if ($nearCells == 2 or $nearCells == 3){
+			$cellStatus = 1;
+		}
+		else{
+			$cellStatus = 0;
+		}
+	}
+	else{
+		if($nearCells == 3){
+			$cellStatus = 1;
+		}
+	}
+	return $cellStatus;
+}
+
+printSpace($space);
+
+while($gameStatus == GAME_RUNNING){
+	$spaceEvaluated = evaluate($space);
+	if($space <> $spaceEvaluated){
+	$space = $spaceEvaluated;
+	printSpace($spaceEvaluated);
+	usleep(200000);
+	}
+	else{
+		$gameStatus = GAME_STOPPED;
+		echo 'GAME OVER.';
+	}
+}
